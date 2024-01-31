8e417608d596cbdc578543762bd45e3e0dae4c1c
Andrew <andrewspurlock@rocketmail.com>
2024-01-30 18:32:45 -0600

    made most event listeners, created filter w/ buttons, and card to dom function

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
-//Setup Utility Functions - RENDERTODOM 
 
+const data = [
+  {
+    name: "Andrew Spurlock",
+    house: "Gryffindor",
+  },
+  {
+    name: "Katee Spurlock",
+    house: "Hufflepuff"
+  },  
+  {
+    name: "Griffy",
+    house: "Gryffindor",
+  },
+  {
+    name: "Wriggy",
+    house: "Hufflepuff"
+  },  
+  {
+    name: "Andrew Spurlock",
+    house: "Ravenclaw",
+  },
+  {
+    name: "Katee Spurlock",
+    house: "Slytherin"
+  },  
+  {
+    name: "Griffy",
+    house: "Gryffindor",
+  },
+  {
+    name: "Wriggy",
+    house: "Hufflepuff"
+  }]
+
+  //Setup Utility Functions - RENDERTODOM 
 renderToDom = (divId, textToRender) => {
   const selectedElement = document.querySelector(divId);
   selectedElement.innerHTML = textToRender;
@@ -15,7 +49,7 @@ const sortingHatBtn = () => {
   <div class="card-body">
     <h1 class="card-title">Welcome to Hogwarts!</h1>
     <p class="card-text">To begin, please click the button below and provide your information. You will be sorted accordingly!</p>
-    <a href="#studentForm" class="btn btn-primary">Start Sorting</a>
+    <button id="sort-btn" type="button" class="btn btn-warning">Start Sorting</button>
   </div>
 </div>
   `
@@ -31,7 +65,7 @@ const studentForm = () => {
     <input class="form-control form-control-lg" type="text" placeholder="Full Name" id="fullName" aria-label="fullName" required>
     <label for="fullName">Full Name</label>
   </div>
-  <button 
+  <button id="sort-btn"
     type="submit" 
     class="btn btn-success" 
   >
@@ -46,26 +80,67 @@ const studentForm = () => {
 const filterButtons = () => {
   const domString = `
   <div class="d-flex flex-wrap justify-content-between my-3">
-    <button class="btn all btn-lg buttonRow" id="All">All</button>
-    <button class="btn gryff btn-lg buttonRow" id="Gryffindor">Gryffindor</button>
-    <button class="btn huff btn-lg buttonRow" id="Hufflepuff">Hufflepuff</button>
-    <button class="btn raven btn-lg buttonRow" id="Ravenclaw">Ravenclaw</button>
-    <button class="btn slyth btn-lg buttonRow" id="Slytherin">Slytherin</button>
+    <button class="btn all btn-lg buttonRow" id="all">All</button>
+    <button class="btn gryff btn-lg buttonRow" id="gryffindor">Gryffindor</button>
+    <button class="btn huff btn-lg buttonRow" id="hufflepuff">Hufflepuff</button>
+    <button class="btn raven btn-lg buttonRow" id="ravenclaw">Ravenclaw</button>
+    <button class="btn slyth btn-lg buttonRow" id="slytherin">Slytherin</button>
   </div>
   `;
   renderToDom('#filterContainer', domString);
 };
 
+//Cards to DOM!
+const cardsOnDom = (array) => {
+  let domString = ''
+  array.map(item => {
+    domString += `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${item.name}</h5>
+      <p class="card-text">${item.house}</p>
+      <button class="btn btn-danger" id="delete--${pet.id}">Expel!</button> 
+    </div>
+  </div>`
+  })
+  renderToDom("#firstYearContainer", domString)
+}
 
 
 
 
 
+// EVENT LISTENERS //
 
+// const eventListeners = () => {
+//   const formCard = new bootstrap.Form(document.querySelector('#sort-btn'));
+// }
+
+//Filter Btn Listeners!
+document.querySelector('#filterContainer').addEventListener('click', (e) => {
+  if(e.target.id === "all") {
+    cardsOnDom(data)
+  } else if (e.target.id === "gryffindor") {
+    const gry = data.filter(taco => taco.house === "Gryffindor");
+    cardsOnDom(gry);
+  } else if (e.target.id === "hufflepuff") {
+    const huf = data.filter(taco => taco.house === "Hufflepuff");
+    cardsOnDom(huf);
+  } else if (e.target.id === "ravenclaw") {
+    const rav = data.filter(taco => taco.house === "Ravenclaw");
+    cardsOnDom(rav);
+  } else if (e.target.id === "slytherin") {
+    const sly = data.filter(taco => taco.house === "Slytherin");
+    cardsOnDom(sly);
+  } 
+});
+
+
+//On Start Functions!
 const startApp = () => {
   sortingHatBtn();
   filterButtons();
-  // eventListeners(); // always last
+  cardsOnDom(data);
+  eventListeners(); // always last
 };
 
 startApp();
