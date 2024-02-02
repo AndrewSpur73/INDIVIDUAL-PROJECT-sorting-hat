48038848df458e61a7174293ce9a0026362ee1e4
Andrew <andrewspurlock@rocketmail.com>
2024-02-01 22:24:18 -0600

    expel button connected, needs update

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ renderToDom = (divId, textToRender) => {
 //Sorting Hat Section w/ button - Needs event listener
 const sortingHatBtn = () => {
   const domString = `
-  <div class="card">
+  <div class="card hat">
     <div class="card-header">
     </div>
   <div class="card-body">
@@ -26,7 +26,7 @@ const sortingHatBtn = () => {
 const studentForm = () => {
   const domString = `
  <form id="inputForm">
- <header>Enter First Year's Name</header>
+ <h3 class="form-header">Enter First Year's Name</h3>
   <div class="form-floating mb-3">
     <input class="form-control form-control-lg" type="text" placeholder="Full Name" id="fullName" aria-label="fullName" required>
     <label for="fullName">Full Name</label>
@@ -43,7 +43,7 @@ const studentForm = () => {
      e.preventDefault();
      
      const newStudentObj = {
-      //  id: students.length +1,
+       id: students.length +1,
        name: document.querySelector ('#fullName').value,
        house: randomHouse()
      }
@@ -86,18 +86,39 @@ const filterButtons = () => {
 
 //Cards to DOM!
 const cardsOnDom = (array) => {
-  let domString = ''
-  array.map(item => {
-    domString += `<div class="card" style="width: 18rem;">
-    <div class="card-body">
-      <h5 class="card-title">${item.name}</h5>
-      <p class="card-text">${item.house}</p>
-      <button class="btn btn-danger" id="expel--${item.id}">Expel!</button> 
-    </div>
-  </div>`
+  let domString = `
+  <div class="first-years">
+    <h1 class="card-title-first">FIRST YEAR STUDENTS</h1>`
+      array.forEach(item => {
+        domString += `<div class="card ${item.house}" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">${item.name}</h5>
+          <p class="card-text">${item.house}</p>
+          <button class="btn btn-danger" id="expel--${item.id}">Expel!</button> 
+          </div>
+        </div>
+    </div>`
   })
   renderToDom("#firstYearCards", domString)
   filterButtons();
+}
+
+//Card background function
+
+
+//Voldy's Army
+const voldyOnDaDom = (army) => {
+  let domString = `
+  <div class="vold-army">
+  <h1 class="card-title-first">Voldemort's Army</h1>`
+  army.forEach(member => {
+    domString += `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${member.name}</h5>
+    </div>
+  </div>`
+  })
+  renderToDom("#expelCards", domString)
 }
 
 
@@ -110,7 +131,18 @@ const eventListeners = () => {
   document.querySelector('#sort-btn').addEventListener('click', () => {
     studentForm()
   });
+  document.querySelector('#firstYearCards').addEventListener('click', (e) => {
+    if (e.target.id) {
+      const[, id] = e.target.id.split('--');
+      const index = students.findIndex(taco => taco.id === id)
 
+      if (e.target.id.includes('expel')) {
+        students.splice(index, 1);
+//https://stackoverflow.com/questions/45919837/move-object-from-one-array-to-another
+        cardsOnDom(students);
+    }
+   };
+  })
 }
 
 //Filter Btn Listeners!
